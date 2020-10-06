4e29d5832820e0e7f65fcf4daa3fe43255f1ccce
Marut Pandya <pandyamarut@gmail.com>
2020-10-06 17:06:46 +0530

    Update clientset.go

---
+++ pkg/kubernetes/clientset.go
@@ -0,0 +1,35 @@
+package kubernetes
+
+import (
+	log "github.com/sirupsen/logrus"
+	"k8s.io/client-go/kubernetes"
+	"k8s.io/client-go/rest"
+)
+
+// RestConfig will be used globally across different packages
+var RestConfig *rest.Config
+
+// ClientSet will be used globally across different packages
+var ClientSet kubernetes.Interface
+
+
+// NewRestConfig will create a new InClusterConfig
+func NewRestConfig() (*rest.Config, error) {
+	restConfig, err := rest.InClusterConfig()
+	if err != nil {
+		log.Error(err)
+		return nil, err
+	}
+	return restConfig, nil
+}
+
+// NewClientSet will create a new k8s ClientSet
+func NewClientSet(config *rest.Config) (kubernetes.Interface, error) {
+	clientSet, err := kubernetes.NewForConfig(config)
+	if err != nil {
+		log.Error(err)
+		return nil, err
+	}
+
+	return clientSet, nil
+}
