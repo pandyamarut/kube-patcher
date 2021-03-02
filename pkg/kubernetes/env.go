78b1da1504ff1c51d499237d83011030be0629e0
Marut Pandya <pandyamarut@gmail.com>
2021-03-02 23:20:46 +0530

    Update env.go

--- pkg/kubernetes/env.go
+++ pkg/kubernetes/env.go
@@ -4,9 +4,11 @@ import (
 	"os"
 )
 
-// Getenv will return enviornment variables
+// Getenv will return enviornment variables,
 func Getenv() map[string]string {
 	return map[string]string{
+		
+		// example, namespace and the resource name to patch the config. 
 		"namespace":    os.Getenv("NAMESPACE"),
 		"resourceName": os.Getenv("APPLICATION_NAME"),
 	}
